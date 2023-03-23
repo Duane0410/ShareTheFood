d0778c2f0f5f533a69c4094c2d33dc2c6e27d88d
Duane Francis Rodrigues <74784006+Duane0410@users.noreply.github.com>
2023-03-24 03:17:37 +0530

    Changes

--- app.py
+++ app.py
@@ -23,32 +23,54 @@ mysql = MySQL(app)
 @app.route("/",methods=["GET","POST"])
 def index():
     if request.method=="GET":
-            return render_template("index.html")
+            cursor=mysql.connection.cursor()
+            cursor.execute('SELECT * FROM donor')
+            mydata=cursor.fetchall();
+            return render_template("index.html",data=mydata)
 
     if request.method=="POST":
+        name=request.form["name"];
+        address=request.form["address"];
+        phone=request.form["phone"];
+        classes=request.form["classes"];
+        name1=request.form["name1"];
+        quantity1=request.form["quantity1"];
             
-            name=request.form["name"];
-            address=request.form["address"];
-            phone=request.form["phone"];
-            classes=request.form["classes"];
-            name1=request.form["name1"];
-            quantity1=request.form["quantity1"];
-            cursor=mysql.connection.cursor()
-            cursor.execute('''Insert INTO donor(Name,Address, Phone_Number,Food_Type,Item_Name,Quantity) Values(%s,%s,%s,%s,%s,%s)''',(name,address,phone,classes,name1,quantity1))
-            mysql.connection.commit();
+        cursor=mysql.connection.cursor()
+        cursor.execute('''Insert INTO donor(Name,Address, Phone_Number,Food_Type,Item_Name,Quantity) Values(%s,%s,%s,%s,%s,%s)''',(name,address,phone,classes,name1,quantity1))
 
-        
+        mysql.connection.commit();
 
+        return render_template("index.html")
 
-    
-            return redirect("/")
+@app.route("/req",methods=["GET","POST"])
+def index1():
+    if request.method=="GET":
+            return render_template("request.html")
+
+    if request.method=="POST":
+        rname=request.form["rname"];
+        mno=request.form["mno"];
+        don=request.form["donation"];
+        add=request.form["add"];
+        needs=request.form["needs"];
+
+        cursor=mysql.connection.cursor();
+        cursor.execute('''INSERT INTO request(Name,No,Address,needs,item_Type) Values(%s,%s,%s,%s,%s)''',(rname,mno,add,needs,don))
+
+        mysql.connection.commit();
+
+    return redirect("/")
 
 
 @app.route("/Donor",methods=["GET","POST"])
 def l():
         
         if request.method=="GET":
-            return render_template("index.html")
+            cursor=mysql.connection.cursor()
+            cursor.execute('SELECT * FROM donor')
+            mydata=cursor.fetchall();
+            return render_template("index.html",data=mydata)
 
         if request.method=="POST":
             
@@ -68,9 +90,10 @@ def l():
 
 
             
-            cursor=mysql.connection.cursor()
+            cursor=mysql.connection.cursor();
+            cursor2=mysql.connection.cursor();
             cursor.execute('''Insert INTO donor(Name,Address, Phone_Number,Food_Type,Item_Name,Quantity) Values(%s,%s,%s,%s,%s,%s)''',(name,address,phone,classes,name1,quantity1))
-            cursor.execute('''Insert INTO Request(Name,Address, No,needs,item_Type) Values(%s,%s,%s,%s,%s)''',(rname,mno,add,needs,don))
+            cursor2.execute('''Insert INTO Request(Name,Address, No,needs,item_Type) Values(%s,%s,%s,%s,%s)''',(rname,mno,add,needs,don))
 
             mysql.connection.commit();
 
